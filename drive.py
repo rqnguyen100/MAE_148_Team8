65cde65240c60ec8f808159719b98fc268f16cb1
rqnguyen100 <75060011+rqnguyen100@users.noreply.github.com>
2022-11-26 16:47:13 -0800

    Added smoothing ability

--- drive.py
+++ drive.py
@@ -31,18 +31,31 @@ def main():
         # Create VESC Object
         # carVESC = VESC('/dev/ttyACM0') 
 
+        # smoothing variables
+        instances = 10 # get n frame values and average to allow for smoothing
+        steering_val = []; throttle_val = [] # array to hold values
+
         while True:
-            # get frame from camera
-            videoIn = video.get()
-            orig_frame = videoIn.getCvFrame()
+            for frame in range(instances):
+                # get frame from camera
+                videoIn = video.get()
+                orig_frame = videoIn.getCvFrame()
 
-            # calculate steering and throttle based on lane_detection
-            steering, throttle = lane_detection.main(orig_frame)
-
-            print(steering, throttle)
+                # calculate steering and throttle based on lane_detection
+                steering, throttle = lane_detection.main(orig_frame)
+                steering_val.append(steering)
+                throttle_val.append(throttle)
+            
+            # finds average value to input into VESC
+            steering_avg = sum(steering_val)/instances
+            throttle_avg = sum(throttle_val)/instances
+            print(steering_avg, throttle_avg)
             
             # Use VESC.run() to set steering and throttle
             # carVESC.run(steering,throttle)
+
+            # reset steering and throttle vals
+            steering_val = []; throttle_val = []
 
 if __name__ == '__main__':
     main()
